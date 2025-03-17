07396121c68ee323b727734ae3df64d7757b160b
Inarastars <inara.aksha@gmail.com>
2025-03-17 16:47:01 +0500

    Add files via upload

---
+++ samsung12.03/main.py
@@ -0,0 +1,40 @@
+#prime_list = [2, 3 , 4 , 5 , 6, 7, 8 , 9, 10]
+#print(prime_list[0])
+
+#лист1 содержит от 2 до 10 числа. и надо добавить число 11.
+#list1 = []
+
+#умножение каждого числа из лист1 на лист2
+#list1 = [3,5,7]
+#list2 = [2,3,4,5,6]
+#for i in list1:
+#    for j in list2:
+#        print(f"{i} * {j} = {i * j}")
+
+#4задача.вывести самый короткий
+#s_list = ["abc", "bcd", "bcdefg", "abba", "cddc", "opq"]
+#print(min(s_list, key=len))
+
+#выевести самое длинное
+#s_list = ["abc", "bcd", "bcdefg", "abba", "cddc", "opq"]
+#print(max(s_list, key=len))
+
+#вывести все короткие числа
+#s_list = ["abc", "bcd", "bcdefg", "abba", "cddc", "opq"]
+#min_len = len(s_list[0])
+#for string in s_list:
+#    if len(string) < min_len:
+#        min_len = len(string)
+#short_str = []
+#for string in s_list:
+#    if len(string) == min_len:
+#        short_str.append(string)
+#print(short_str)
+
+#7 задача
+capital_dic = {
+    "Korea": "Seoul",
+    "Chine": "Beijing",
+    "USA": "Washington DC",
+}
+print(capital_dic["Korea"])
