a0ebd431d089b06883959c1f313aa96112737594
Inarastars <inara.aksha@gmail.com>
2025-03-17 16:46:04 +0500

    Add files via upload

---
+++ samsung5/main.py
@@ -0,0 +1,49 @@
+#код проверяет если в а частичка б
+# a = int(input("введите букву"))
+# b = "aeiou"
+# if a not in b:
+#   print("it is a consonant")
+# else:
+#   print(" it is a vowel")
+
+#код проверяет делится ли число а на б.
+# a = int(input("введите чилсо"))
+# b = int(input("введите чилсо"))
+# if a % b == 0:
+#   print("делится")
+# else:
+#    print("не делится")
+
+
+#калькулятор, выполняющий возведение в степень, деление, суммирование, вычитание и умножение
+#c = input("введите команду")
+#a = int(input("введите число"))
+#b = int(input("введите число"))
+#if c == "+":
+ #  print(a+b)
+#elif c == "*":
+ #   print(a*b)
+#elif c == "**":
+ #   print(a**b)
+#elif c == "-":
+ #   print(a-b)
+#else:
+ #   print(a/b)
+
+
+
+#спрашивает координаты х и у, и определяет в какой четверти находится
+#x = int(input("введите координату х:"))
+#y = int(input("введите координату у:"))
+#if x > 0 and y > 0:
+ #   print("in the 1 quadrant")
+#elif x < 0 and y > 0:
+ #   print("in the 2 quadrant")
+#elif x < 0 and y < 0:
+#    print(" in the 3 quadrant")
+#else:
+   # print("in the 4 quadrant")
+
+
+#print("Welcome to the yummy restaurant. Here is the menu. ")
+#print("for choose burger, enter 'b'")
