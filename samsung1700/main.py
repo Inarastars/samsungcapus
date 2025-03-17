61ebd6178d8291c3fbfd21ff9776cde26ca65a7c
Inarastars <inara.aksha@gmail.com>
2025-03-17 16:45:23 +0500

    Add files via upload

---
+++ samsung1700/main.py
@@ -0,0 +1,31 @@
+#bts = ["V", "J-Hope", "RM", "Jungkook","Jin","Jimin","Suga"]
+#for i in range(len(bts)):
+ #   print(bts[i])
+
+#print(sum(range (1,101)))
+
+#s = 0
+#for i in range(1,101):
+ #   s += i
+#print(s)
+
+#s = sum(range(2,101,2))
+#print(s)
+
+#s = sum(range(1,101,2))
+#print(s)
+
+#n = int(input("скока мест "))
+#num = 1
+#for i in range(n):
+ #   if i % 2 == 0:
+  #      for j in range(n):
+   #         print(num, end=" ")
+    #        num +=1
+    #else:
+     #   num -= n - 1
+      #  for j in range(n):
+       #     print(num,end=" ")
+        #    num -=1
+        #num += n +1
+    #print()
